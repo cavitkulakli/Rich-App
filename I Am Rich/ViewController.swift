163c4302eee695e5719b07346afd3b353be04610
Cavit KULAKLI <15360190+cavitkulakli@users.noreply.github.com>
2018-11-25 19:45:52 +0300

    Basic Rich Application

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Cavit on 18.11.2018.
+//  Copyright © 2018 Cavit KULAKLI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,24 @@
+## Getting Started
+
+Basic swift application
+
+
+
+No auto layout
+
+```
+Best for Iphone 8 Plus
+```
+
+
+### Built With
+
+Only you need to change bundle identifier
+
+```
+com.yourname.I-Am-Rich
+```
+
+Set your team and build
+
+
